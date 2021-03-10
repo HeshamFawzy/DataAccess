7949adfc6681c9d0e02e2d4732966f3f30d1e387
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2021-03-10 10:56:01 +0200

    Add project files.

---
+++ DataAccess/Models/EmployeeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAccess.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int ConfirmEmail { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
